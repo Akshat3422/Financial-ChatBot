ae22d0b494911643ffaa2973caf9992ed9346f30
Akshat Gupta <akshat4899@gmail.com>
2025-12-19 04:41:20 +0530

    query retrieve

--- src/utils/retrieve_context.py
+++ src/utils/retrieve_context.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 from langchain_pinecone import PineconeVectorStore
 from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 from ..utils.pinecone_initializer import initialize_pinecone
-from ...src.rag.retriever import get_retriever
+from ..rag.retriever import get_retriever
 
 load_dotenv()
 
 embedding=GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-index=initialize_pinecone(index_name=os.getenv("INDEX_NAME"))
+index=initialize_pinecone(index_name=os.getenv("PINECONE_INDEX_NAME"))
 
-vectorstore = get_retriever()
+vectorstore = get_retriever(index=index, embedding=embedding)
 
 
 def retrieve_context(query: str):

690f6ac957aa79a345151218d07db7569b715a8f
Akshat Gupta <akshat4899@gmail.com>
2025-12-19 03:28:23 +0530

    chunking completed

--- src/rag/chunking.py
+++ src/rag/chunking.py
@@ -1,17 +1,20 @@
 from ..utils.document_loader import load_documents
 from ..utils.logger import get_logger
+from ..utils.chunk import chunk_documents
+
+
+
+
 logger = get_logger(__name__)
-
-
-
-
 # Path of the documents directory
 path=r"C:\Users\user\Desktop\Finance_RAG\documents"
 
 documents = load_documents(path)
+chunks = chunk_documents(documents)
+
 logger.info(f"Loaded {len(documents)} documents from {path}")
 
-logger.info("Document loading completed.")
+logger.info(f"Chunked documents into {len(chunks)} chunks.")
 
 
 
